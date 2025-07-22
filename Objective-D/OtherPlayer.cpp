c329e14b31f8968ef55c035a6986c1b5fe31fd63
TU-PJY <viska0319@icloud.com>
2025-07-22 15:16:46 +0900

    타 플레이어 선형보간 속도 상향

--- Objective-D/OtherPlayer.cpp
+++ Objective-D/OtherPlayer.cpp
@@ -60,8 +60,8 @@ void OtherPlayer::updateAnimation(float Delta) {
 }
 
 void OtherPlayer::updateRenderValue(float Delta) {
-	Math::LerpXMFLOAT3(position, positionDest, 10.0, Delta);
-	Math::LerpXMFLOAT3(rotation, rotationDest, 10.0, Delta);
+	Math::LerpXMFLOAT3(position, positionDest, 20.0, Delta);
+	Math::LerpXMFLOAT3(rotation, rotationDest, 20.0, Delta);
 }
 
 void OtherPlayer::updateBound() {
