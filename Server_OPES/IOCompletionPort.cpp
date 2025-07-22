da2503dd270b6c168f24fc47c0748accbef7dc83
hndd0725 <134517174+hndd0725@users.noreply.github.com>
2025-07-22 16:28:46 +0900

    임시객체-roomHp
    디펜스 모드일때 필요한 객체

--- Server_OPES/IOCompletionPort.cpp
+++ Server_OPES/IOCompletionPort.cpp
@@ -20,7 +20,8 @@ ScriptUtil monsterDataScript;
 // 현재 로드된 몬스터 생성 타입 및 위치
 std::vector<MonsterData> monsterData;
 
-std::vector<MonsterData> myMonsters;
+std::vector<MonsterData> myMonsters;//임시
+int roomHp;//임시
 
 std::thread npcThread;
 
@@ -1157,45 +1158,6 @@ void IOCompletionPort::WorkThread() {
                     }
                 }
             }
-            //else if (*packetType == PacketType::PTOM_DAMAGE) {
-            //    PtoMDamagePacket* pkt = reinterpret_cast<PtoMDamagePacket*>(pOverlappedEx->buffer);
-            //
-            //    std::cout << "PTOM_DAMAGE Mid:" << pkt->monsterID
-            //        << ", Pid:" << pkt->playerID
-            //        << ", Attackhp:" << pkt->attackHp << std::endl;
-            //
-            //    // 해당 클라이언트의 방 정보에서 몬스터 찾기
-            //    //auto it = rooms.find(client->roomID);
-            //    //if (it != rooms.end()) {
-            //    //    Room& room = it->second;
-            //    //
-            //    //    // 몬스터 ID가 벡터 범위 내에 있는지 확인
-            //    //    if (pkt->monsterID < room.monsters.size()) {
-            //    //        MonsterData& monster = room.monsters[pkt->monsterID];
-            //    //
-            //    //        monster.hp -= pkt->attackHp;
-            //    //        if (monster.hp < 0) monster.hp = 0;
-            //    //
-            //    //        std::cout << "[HP 반영] 몬스터 ID " << monster.id
-            //    //            << " 새 HP: " << monster.hp << std::endl;
-            //    //
-            //    //        // 데미지 정보를 같은 방의 다른 클라이언트들에게 전송
-            //    //        for (stClientInfo* otherClient : clients) {
-            //    //            if (!otherClient) continue;
-            //    //            if (otherClient->roomID == client->roomID && otherClient != client) {
-            //    //                SendData_PtoMDamagePacket(otherClient,pkt->playerID,pkt->monsterID, pkt->attackHp);
-            //    //            }
-            //    //        }
-            //    //    }
-            //    //    else {
-            //    //        std::cerr << "[경고] 잘못된 monsterID: " << pkt->monsterID
-            //    //            << ", monsters.size(): " << room.monsters.size() << std::endl;
-            //    //    }
-            //    //}
-            //    //else {
-            //    //    std::cerr << "[에러] roomID " << client->roomID << "에 해당하는 방을 찾을 수 없음\n";
-            //    //}
-            //}
 
             else if (*packetType == PacketType::MTOP_DAMAGE) {
 
@@ -1248,11 +1210,12 @@ void IOCompletionPort::WorkThread() {
 
                 CenterBuildingPacket* pkt = reinterpret_cast<CenterBuildingPacket*>(pOverlappedEx->buffer);
 
+                roomHp -= pkt->hp;
                 // 데미지 패킷을 모든 클라이언트에게 전송
                 for (stClientInfo* otherClient : clients) {
                     if (!otherClient) continue;
                     if (otherClient != client /*&& client->roomID == otherClient->roomID*/) { // 패킷을 보낸 클라이언트에게는 다시 전송하지 않음
-                        SendData_CenterBuildingPacket(otherClient, pkt->hp);
+                        SendData_CenterBuildingPacket(otherClient, roomHp);
                     }
                 }
             }
