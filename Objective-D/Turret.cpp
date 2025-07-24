738ead389b6b8b2f2ce77c003c8d007b766559f6
TU-PJY <viska0319@icloud.com>
2025-07-24 13:47:25 +0900

    plantmonster가 땅 밑에서 죽으면 사라지지 않는 문제 수정

--- Objective-D/Turret.cpp
+++ Objective-D/Turret.cpp
@@ -51,7 +51,7 @@ void Turret::Update(float Delta) {
 	size_t size = scene.LayerSize(LAYER_MONSTER);
 	for (int i = 0; i < size; i++) {
 		if (auto monster = scene.ReferLayer(LAYER_MONSTER, i); monster) {
-			if (!monster->GetDeathState() && monster->CheckHit(lookRange)) {
+			if (!monster->GetDeathState() && monster->CheckHit(lookRange) && monster->GetBehaviorState()) {
 				xmfloat3 lookPosition = monster->GetPosition();
 				Ray newRay = Math::CalcRayVector(position, lookPosition);
 
