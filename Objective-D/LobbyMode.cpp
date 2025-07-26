7cd25a0f91b1fed8f08f6e7434e8c93be84ee35e
TU-PJY <viska0319@icloud.com>
2025-07-26 14:52:57 +0900

    Lobby 모드 추가

---
+++ Objective-D/LobbyMode.cpp
@@ -0,0 +1,14 @@
+#include "ModePack.h"
+#include "Lobby.h"
+
+namespace LobbyMode { std::deque<GameObject*> ControlObjectList; }
+
+void LobbyMode::Start() {
+	scene.SetupMode("LobbyMode", Destructor, ControlObjectList);
+	// add works here
+
+	scene.AddObject(new Lobby, "lobby", LAYERUI, true);
+}
+
+void LobbyMode::Destructor() {
+}
--- Objective-D/ModePack.h
+++ Objective-D/ModePack.h
@@ -18,6 +18,11 @@ namespace Level3 {
 	void Destructor();
 }
 
+namespace LobbyMode {
+	void Start();
+	void Destructor();
+}
+
 // FBX 애니메이션 구현을 위한 테스트 모드. 실제 개발 시 비활성화한다.
 namespace TestMode {
 	void Start();
