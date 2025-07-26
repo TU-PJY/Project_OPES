62c33fa466a88997c3d91805b1780f8d36f4fb5d
TU-PJY <viska0319@icloud.com>
2025-07-26 11:47:25 +0900

    수류탄 대미지가 0을 넘을 떄만 패킷을 보내도록 수정

--- Objective-D/Grenade.cpp
+++ Objective-D/Grenade.cpp
@@ -232,9 +232,11 @@ void Grenade::Update(float Delta) {
                         float t = (15.0 - distance) / 15.0;
                         t = std::clamp(t, 0.0f, 1.0f);
                         int damage = (int)(300.0 * t);
-                        if(!GLOBAL.useServer)
+                        if(!GLOBAL.useServer && damage > 0)
                             monster->GiveDamage(damage);
-                        SendPtoMDamagePacket(monster->GetID(), damage);
+
+                        if(damage > 0)
+                            SendPtoMDamagePacket(monster->GetID(), damage);
                     }
                 }
             }
