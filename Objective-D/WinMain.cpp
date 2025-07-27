b9a9bcd8ee99f2a0518366107dea964a53c26a59
hndd0725 <134517174+hndd0725@users.noreply.github.com>
2025-07-28 00:55:16 +0900

    2stage 확인

--- Objective-D/WinMain.cpp
+++ Objective-D/WinMain.cpp
@@ -75,12 +75,12 @@ struct RecvContext {
 
 // 인게임 모드로 스킵하려면 true
 // StartMod도 Level1 ~ Level3으로 변경해주어야 함
-constexpr bool skipTitleMode = true;
+constexpr bool skipTitleMode = false;
 
 constexpr bool skipDefenseMode = false;
 constexpr bool editMode = false;
 
-constexpr bool useServer = false;//클라만 켜서 할땐 false로 바꿔서하기
+constexpr bool useServer = true;//클라만 켜서 할땐 false로 바꿔서하기
 constexpr bool localServer = false; //!useServer;
 
 // 개발 시 로드 시간 단축을 위해 선택적으로 리소스를 로드할 수 있도록 하였다.
@@ -95,7 +95,7 @@ bool LoadMap2Resources = true;
 bool LoadMap3Resources = false;
 
 // 시작 모드
-START_MODE_PTR StartMode = Level1::Start;
+START_MODE_PTR StartMode = TitleMode::Start;
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
