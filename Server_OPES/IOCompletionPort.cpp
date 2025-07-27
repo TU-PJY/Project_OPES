e84d0498571fcbd6485f9b294b578a691c601e9a
hndd0725 <134517174+hndd0725@users.noreply.github.com>
2025-07-28 06:09:53 +0900

    0.1초 후 지운다

--- Server_OPES/IOCompletionPort.cpp
+++ Server_OPES/IOCompletionPort.cpp
@@ -745,7 +745,7 @@ void IOCompletionPort::PostRemove(stClientInfo* client) {
 
     DelayedRemoveEntry entry;
     entry.client = client;
-    entry.timeScheduled = std::chrono::steady_clock::now() + std::chrono::milliseconds(1000); // 100ms 후 제거
+    entry.timeScheduled = std::chrono::steady_clock::now() + std::chrono::milliseconds(100); // 100ms 후 제거
 
     std::lock_guard<std::mutex> lock(removeQueueMutex);
     removeQueue.push_back(entry);
