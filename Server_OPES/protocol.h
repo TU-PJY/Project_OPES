26080cf8e73709f6533ab62dd146e9a988ef4df4
TU-PJY <viska0319@icloud.com>
2025-07-28 11:17:49 +0900

    push

--- Server_OPES/protocol.h
+++ Server_OPES/protocol.h
@@ -81,7 +81,7 @@ constexpr int                  DEFENSE_MONSTER2 = 5;
 constexpr int                  DEFENSE_MONSTER3 = 5;
 
 //start 인원수 
-constexpr int				   MIN_PLAYER_COUNT = 2;          
+constexpr int				   MIN_PLAYER_COUNT = 3;          
 
 //스테이지 넘어갈때 딜레이 시간
 constexpr int                 PASS_STAGE_TIME = 5;
