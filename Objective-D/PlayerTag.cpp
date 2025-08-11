d7d0f48af087b53481775ca0e89f3e1736a5fa85
TU-PJY <viska0319@icloud.com>
2025-08-11 15:49:52 +0900

    플레이어 닉네임 태그 위치가 잘못된 값을 사용하는 문제 수정

--- Objective-D/PlayerTag.cpp
+++ Objective-D/PlayerTag.cpp
@@ -11,18 +11,16 @@ PlayerTag::PlayerTag(const std::string& name) {
 
 void PlayerTag::Render() {
 	Vector vec{};
-	float renderMultiply = Math::CalcDistance3D(XMFLOAT3(0.0, 6.0, 0.0), camera.GetPosition()) * 0.5;
+	XMFLOAT3 renderPosition = XMFLOAT3(position.x, position.y + 6.0, position.z);
+	float renderMultiply = Math::CalcDistance3D(renderPosition, camera.GetPosition()) * 0.5;
 
 	BeginRender();
-	Transform::Move(TranslateMatrix, position);
-	Math::BillboardLookAt(RotateMatrix, vec, position, camera.GetPosition());
-
-	Transform::Move(TranslateMatrix, 0.0f, 6.0f, 0.0f);
-	Math::BillboardLookAt(RotateMatrix, vec, XMFLOAT3(0.0f, 6.0f, 0.0f), camera.GetPosition());
+	Transform::Move(TranslateMatrix, renderPosition);
+	Math::BillboardLookAt(RotateMatrix, vec, renderPosition, camera.GetPosition());
 	Transform::Scale(ScaleMatrix, 0.03 * 8.0 * renderMultiply, 0.05 * renderMultiply, 1.0);
 	Render3D(SYSRES.BillboardMesh, TEX.ColorTex, 0.6, DEPTH_TEST_NONE);
 
-	tagText.Render3D(XMFLOAT3(position.x, position.y + 6.0, position.z), 0.03, tag);
+	tagText.Render3D(renderPosition, 0.03, tag);
 }
 
 void PlayerTag::InputPosition(XMFLOAT3& Position) {
