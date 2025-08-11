5c3c6539dae6d73437faa6e89b6e400b5033c3c3
TU-PJY <viska0319@icloud.com>
2025-08-11 14:40:08 +0900

    push

--- Objective-D/WinMain.cpp
+++ Objective-D/WinMain.cpp
@@ -78,10 +78,10 @@ struct RecvContext {
 // StartMod도 Level1 ~ Level3으로 변경해주어야 함
 constexpr bool skipTitleMode = false;
 
-constexpr bool skipDefenseMode = true;
+constexpr bool skipDefenseMode = false;
 constexpr bool editMode = false;
 
-constexpr bool useServer = false;//클라만 켜서 할땐 false로 바꿔서하기
+constexpr bool useServer = true;//클라만 켜서 할땐 false로 바꿔서하기
 constexpr bool localServer = false; //!useServer;
 
 // 개발 시 로드 시간 단축을 위해 선택적으로 리소스를 로드할 수 있도록 하였다.
