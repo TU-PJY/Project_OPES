857d1aece92fa8a8e41994c8712699584e7235ec
TU-PJY <viska0319@icloud.com>
2025-08-16 16:31:01 +0900

    설정 변경

--- Objective-D/WinMain.cpp
+++ Objective-D/WinMain.cpp
@@ -74,12 +74,12 @@ struct RecvContext {
 
 // 인게임 모드로 스킵하려면 true
 // StartMod도 Level1 ~ Level3으로 변경해주어야 함
-constexpr bool skipTitleMode = true;
+constexpr bool skipTitleMode = false;
 
-constexpr bool skipDefenseMode = true;
+constexpr bool skipDefenseMode = false;
 constexpr bool editMode = false;
 
-constexpr bool useServer = false;//클라만 켜서 할땐 false로 바꿔서하기
+constexpr bool useServer = true;//클라만 켜서 할땐 false로 바꿔서하기
 constexpr bool localServer = false; //!useServer;
 
 // 개발 시 로드 시간 단축을 위해 선택적으로 리소스를 로드할 수 있도록 하였다.
@@ -90,17 +90,17 @@ bool DevMode = true;
 bool UIcreateMode = false;
 
 bool LoadMap1Resources = true;
-bool LoadMap2Resources = false;
-bool LoadMap3Resources = false;
+bool LoadMap2Resources = true;
+bool LoadMap3Resources = true;
 
 // 사운드 리소스를 로드한다.
 bool LoadSoundResources = false;
 
 // 시작 모드
-START_MODE_PTR StartMode =  Level1::Start;
+START_MODE_PTR StartMode =  TitleMode::Start;
 
 // 0: heavy, 1: dmr, 2: engineer
-int myCharacter = 1;
+int myCharacter = 0;
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
